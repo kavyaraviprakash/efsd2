569f9d5f6543527d4a24b499014ceba6ddc178e2
kayaraviprakash <kraviprakash@unomaha.edu>
2021-07-26 14:58:27 +0530

    Commiting changes for efsd2

---
+++ efs/efsusers/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'is_customer', 'is_FinancialAdvisor', 'is_superuser']
+    fieldsets = ((None, {'fields': ('username', 'password')}),
+                 ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+                 ('Permissions', {'fields': ('is_customer', 'is_staff', 'is_active', 'is_FinancialAdvisor',)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ efs/efsusers/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=25)
+    is_customer = models.BooleanField('customer', default=True)
+    is_FinancialAdvisor = models.BooleanField('FinancialAdvisor', default=False,
+                                              help_text='check it if you want to sign up as Financial Advisor')
+
+    def __str__(self):
+        return '{}'.format(self.username)
